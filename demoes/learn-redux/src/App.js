1f815c51788e0ee5cce8fc7aaea6506443e4a8c0
ifyour <ifyour@outlook.com>
2018-03-04 14:53:22 +0800

    redux example

--- demoes/learn-redux/src/App.js
+++ demoes/learn-redux/src/App.js
@@ -1,82 +1,36 @@
-import React from 'react';
-import { Button,List } from 'antd-mobile';
+import React from "react";
+import { connect } from 'react-redux';
+import { addgun, removegun, addgunAsyac } from './index.redux'
+
+// 使用 babel 插件 transform-decorators-legacy , 采用装饰器写法
+@connect(
+    // 需要什么 state 放入到 props 里
+    state => ({num: state}),
+    // 有哪些变更 state 的 action 方法, 这些都能自动进行 dispatch 任务(React-redux 包装后)
+    { addgun, removegun, addgunAsyac }
+)
 
 class App extends React.Component {
-  render() {
-    const boss = 'jack'
-    return (
-      <div>
-        <h1>独立团：{boss}</h1>
-        <Title />
-      </div>
-    )
-  }
-}
-
-class Title extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      names: ['aaa', 'bbb', 'ccc', 'ddd']
+    render() {
+        const { num, addgun, removegun, addgunAsyac } = this.props; 
+        return (
+            <div>
+                <h1>机枪{ num }把!</h1>
+                <button onClick={ addgun }>申请武器</button>
+                <button onClick={ removegun }>回收武器</button>
+                <button onClick={ addgunAsyac }>延迟 1s 增加 1 把武器</button>
+            </div>
+        )
     }
-  }
-
-  add(){
-    this.setState({
-      names: [...this.state.names, '路人甲' + Math.random()]
-    })
-  }
-
-  render() {
-    let names = this.state.names;
-    return (
-      <div>
-        <Button type="primary" onClick={()=>this.add()}>add</Button>
-        
-        <List renderHeader={()=>"员工列表"}>
-          {
-            names.map(v => {
-              return <List.Item key={v}>{v}</List.Item>
-            })
-          }
-        </List>
-      </div>
-    )
-  }
 }
 
-export default App
+// 对比上面 @connect 的写法, 上面采用了 babel 插件来优化 connect 写法
 
+// // 把 state 转成 props, 回调函数, 接收 state
+// const mapStatetoProps = state =>({num: state});
+// // 定义 redux 需要的 action
+// const actionCreaters = { addgun, removegun, addgunAsyac };
+// // 使用 react-redux 装饰模式, 先执行connect 方法, 返回一个包装函数, 然后包装 App 组件
+// App = connect(mapStatetoProps, actionCreaters)(App);
 
-
-
-
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// // import {createStore} from 'redux';
-// import './es6';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to Imooc</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
+export default App;
--- demoes/learn-redux/src/index.js
+++ demoes/learn-redux/src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import { createStore, applyMiddleware, compose } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// Redux 默认只能处理同步, 异步任务需要 react-thunk 中间件
+import thunk from 'redux-thunk';
+
+// react-redux 是用来优雅连接  react 和 redux 的中间件
+import { Provider } from 'react-redux';
+
+
+import { counter } from './index.redux.js';
+import App from "./App";
+
+// 使用 redux 提供的 applyMiddleware 来处理异步
+// compose 用来混合 2 个函数
+const store = createStore(counter, compose(
+    applyMiddleware(thunk), 
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+ReactDom.render(
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+); 
