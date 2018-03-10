a63dd948f1b360a68b5e92046b57dc52ccde7a4e
王明明 <ifyour@outlook.com>
2018-03-10 11:25:00 +0800

    type test

--- react-job/src/component/avatar-selector/avatar-selector.js
+++ react-job/src/component/avatar-selector/avatar-selector.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid, List } from 'antd-mobile';
 
 class AvatarSelector extends React.Component {
+    static PropTypes = {
+        avatarSelector: PropTypes.func.isRequired
+    }
     constructor(props) {
         super(props);
         this.state = {}
