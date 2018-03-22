2d4ea7724c231f66c59e0383275d1afdd2407a51
ifyour <ifyour@outlook.com>
2018-03-22 09:05:27 +0800

    fix typo

--- react-job/src/component/authrouter/authrouter.js
+++ react-job/src/component/authrouter/authrouter.js
@@ -11,7 +11,7 @@ import { loadUserInfo } from '../../redux/user.redux';
 // 3) 用户的身份 type 是牛人还是 BOSS
 // 4) 用户是否完善了头像
 
-// 当前组件不是 Route 组件, 所以需要使用 react-touter-dom 中的 withRouter 装饰器
+// 当前组件不是 Route 组件, 所以需要使用 react-router-dom 中的 withRouter 装饰器
 // 把路由相关的方法带入到当前组件的属性中
 @withRouter
 @connect(
@@ -29,7 +29,7 @@ class AuthRouter extends React.Component {
             .then(res => {
                 if(res.status === 200 ) {
                     if (res.data.code === 0) {
-                        // 已登录则 用户的类型相关信息放入 redux state 中
+                        // 已登录则 用户的类型相关信息放入 redux store 中
                         this.props.loadUserInfo(res.data.data);
                         // this.props.history.push(getRedirectPath(res.data.data));
                     } else {
